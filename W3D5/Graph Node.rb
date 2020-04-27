b4c874dae93f03688ecfada9a7a449a6e43cfbf4
yuk fai chan <yukfaichan92@gmail.com>
2020-04-26 21:13:42 -0700

    W3D5 Homework

---
+++ W3D5/Graph Node.rb
@@ -0,0 +1,12 @@
+class GraphNode
+  attr_accessor :val, :neighbors
+
+  def initialize(val)
+    self.val = val
+    self.neighbors = []
+  end
+
+  def add_neighbor(node)
+    self.neighbors << node
+  end
+end
